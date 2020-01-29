a550a55dae011bae5891c5bbaca6770cfd63e0ef
artemerbulatov <art_97@bk.ru>
2020-01-30 01:34:18 +0400

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ import mm from './mm.js';
 
 import 'svgxuse';
 import 'web-animations-js';
+import Swiper from 'swiper';
 import lozad from 'lozad';
 import objectFitImages from 'object-fit-images';
 
@@ -25,14 +26,75 @@ $(() => {
 		$('body').addClass('msie');
 	}
 
-	const $container = $('.content:last-child');
+	const $container = $('body');
 
-	if ($container.find('.question').length > 0) {
-		$container.find('.question').each((i, el) => {
-			const $el = $(el);
-			$el.on('click', () => {
-				alert();
-			});
+	if ($container.find('.slider-main').length > 0) {
+		new Swiper('.slider-main', {
+			loop: true,
+
+			pagination: {
+				el: '.swiper-pagination'
+			},
+
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+
+			scrollbar: {
+				el: '.swiper-scrollbar'
+			}
+		});
+	}
+
+	if ($container.find('.slider').length > 0) {
+		let mySwiper = undefined;
+
+		const initSwiper = () => {
+			const screenWidth = $(window).width();
+
+			if (screenWidth < 1000 && mySwiper === undefined) {
+				$('.slider').removeClass('slider_destroy');
+				mySwiper = new Swiper('.slider', {
+					navigation: {
+						nextEl: '.swiper-button-next',
+						prevEl: '.swiper-button-prev'
+					}
+				});
+			} else if (screenWidth > 1000 && mySwiper !== undefined) {
+				mySwiper.destroy();
+				mySwiper = undefined;
+				$('.slider').addClass('slider_destroy');
+				$('.swiper-wrapper').removeAttr('style');
+				$('.swiper-slide').removeAttr('style');
+			}
+		};
+
+		initSwiper();
+
+		$(window).on('resize', function() {
+			initSwiper();
+		});
+	}
+
+	if ($container.find('.slider-reviews').length > 0) {
+		new Swiper('.slider-reviews', {
+			loop: true,
+
+			pagination: {
+				el: '.swiper-pagination'
+			},
+
+			// Navigation arrows
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+
+			// And if we need scrollbar
+			scrollbar: {
+				el: '.swiper-scrollbar'
+			}
 		});
 	}
 });
